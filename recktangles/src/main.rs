5c597b69dcc2ddf2ab4dcd86b41182eb0c4294b5
Svyat <traher74@Svyats-MacBook-Pro.local>
2024-03-22 14:57:56 +0400

    Use Rectangle struct with impl methods

--- recktangles/src/main.rs
+++ recktangles/src/main.rs
@@ -5,20 +5,51 @@ struct Rectangle {
     height: u32,
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
+//fn area(rectangle: &Rectangle) -> u32 {
+//    rectangle.width * rectangle.height
+//}
+
 fn main() {
-    let scale = 2;
+    //let scale = 2;
+    //let rect1 = Rectangle {
+    //    width: dbg!(30 * scale),
+    //    height: 50,
+    //};
+    //dbg!(&rect1);
+    
+    //println!("rect1 is {:?}", rect1);
+    
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
-    dbg!(&rect1);
-    //println!(
-    //    "The area is {}",
-    //    area(&rect1)
-    //);
-    //println!("rect1 is {:?}", rect1);
+    println!(
+        "The area is {}",
+        rect1.area()
+    );
+
+    // Check if can hold another Rectangle
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    println!("Check if rect1 can hold rect2: {}", rect1.can_hold(&rect2)); 
 }
